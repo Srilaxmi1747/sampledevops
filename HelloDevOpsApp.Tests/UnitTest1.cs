5008903721c5e749c77b6d20e3299ad5591321d2
Srilaxmi1747 <srilaxmi.veeramalla@gmail.com>
2025-12-12 11:59:41 +0530

    Added SonarCloud & Coverlet CI/CD

---
+++ HelloDevOpsApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace HelloDevOpsApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(5, 3 + 2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloDevOpsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to DevOps CI/CD Pipeline!");
+        }
+    }
+}
